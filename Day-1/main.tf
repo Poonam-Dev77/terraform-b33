7dd8ad7fd6910d2c4cf74fef06796f152668f038
Poonam-Dev77 <shelakepoonam33@gmail.com>
2026-09-01 13:45:14 +0530

    terraform

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
    ami= "ami-0532913178263be11"  
    instance_type = "t3.micro"
    key_name = "ohio_key"
-   vpc_security_group_ids = "sg-0c447262392a165d4"
+   vpc_security_group_ids = ["sg-0c447262392a165d4"]
    tags = {
       Name = "my_ec2"
    }
