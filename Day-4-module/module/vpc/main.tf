05518751e8b920b8071a2374f75306e5218ffd35
Poonam-Dev77 <shelakepoonam33@gmail.com>
2026-09-06 14:21:10 +0530

    terraform

--- Day-4-module/module/vpc/main.tf
+++ Day-4-module/module/vpc/main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "my_vpc" {
     }
 }
 
-resource "aws_subnet" "my_subent" {
+resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = var.public_subnet_cidr
     availability_zone = var.public_az
