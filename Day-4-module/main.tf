0de62bb76f2a7068348d90466a6fa6fa890d6db9
Poonam-Dev77 <shelakepoonam33@gmail.com>
2026-09-06 14:36:13 +0530

    terraform

--- Day-4-module/main.tf
+++ Day-4-module/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
 
 module "ec2" {
     source = "./module/ec2"
-    ami = "ami-0bea529386a62a2ad"
+    ami = "ami-02159ad7e38d562f2"
     instance_type = "t3.micro"
     key_name = "ohio-key"
     sg_id = module.vpc.sg_id
